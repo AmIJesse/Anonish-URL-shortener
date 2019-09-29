3ad361c3111bd4241f9c079d0ee66f48086837f4
Jesse <jesse@buffer1024.com>
2019-09-29 16:54:12 -0600

    Initial Commit

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+
+	"github.com/rapidloop/skv"
+)
+
+var store *skv.KVStore
+var err error
+
+func main() {
+	// open the store
+	store, err = skv.Open("sessions.db")
+	if err != nil {
+		panic(err)
+	}
+	defer store.Close()
+
+	r := mux.NewRouter()
+	//Set up endpoints
+	r.HandleFunc("/", index).Methods("GET")
+	r.HandleFunc("/add", addRedirect).Methods("POST")
+	r.HandleFunc("/{key}", redirect).Methods("GET")
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  5 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	indexHTML, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(indexHTML)
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	redirectKey := vars["key"]
+
+	var redirectTo string
+	err := store.Get(redirectKey, &redirectTo)
+	if err != nil || redirectTo == "" {
+		http.Redirect(w, r, "https://anon.ish/", http.StatusSeeOther)
+		return
+	}
+
+	http.Redirect(w, r, redirectTo, http.StatusSeeOther)
+	return
+}
+
+func addRedirect(w http.ResponseWriter, r *http.Request) {
+	redirectKey := r.FormValue("key")
+	redirectTo := r.FormValue("to")
+
+	if redirectKey == "" || redirectTo == "" {
+		w.Write([]byte("Missing parameters"))
+		return
+	}
+
+	err := store.Put(redirectKey, redirectTo)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "https://anon.ish/", http.StatusSeeOther)
+	return
+
+}
